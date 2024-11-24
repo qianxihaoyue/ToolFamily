7abdfb43834dd6fd4377eeaf497a95cc3bb1249e
qianxihaoyue <728534126@qq.com>
2024-11-24 12:43:21 +0800

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    ,screenshotwidget_1(nullptr)
+{
+    ui->setupUi(this);
+    QAction* actionScreenShot=ui->toolBar->findChild<QAction*>("actionScreenShot");
+    connect(actionScreenShot,&QAction::triggered,this,&MainWindow::on_actionScreenShot_triggered);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionScreenShot_triggered()
+{
+    if(screenshotwidget_1!=nullptr){
+        screenshotwidget_1->deleteLater();
+        screenshotwidget_1=nullptr;
+    }
+    screenshotwidget_1=new ScreenShotWidget();
+    screenshotwidget_1->showFullScreen();
+}
+
